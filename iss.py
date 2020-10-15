dc4474886daaea1d7c893bf75a03f96e628a27fd
Meagan Ramey <meagan.ramey@yahoo.com>
2020-10-15 11:34:43 -0400

    Created, tested, and completed an astronauts function to retrieve the names and number of astronauts currently in space.

--- iss.py
+++ iss.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Meagan Ramey'
+
+import requests
+
+
+def astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    r = r.json()
+    astronaut_list = r['people']
+    for astronaut in astronaut_list:
+        for k, v in astronaut.items():
+            if k == 'name':
+                print(f'{v} is on the ', end='')
+            else:
+                print(f'{v}')
+    print(f'There are a total of {len(astronaut_list)} in space.')
+
 
 
 def main():
-    pass
+    astronauts_on_ISS = astronauts()
 
 
 if __name__ == '__main__':
