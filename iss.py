5bb677a4915fcb657d2006a347b0a4330b7eed82
Meagan Ramey <meagan.ramey@yahoo.com>
2020-10-21 10:50:24 -0400

    Created a function to get the next time the ISS will be over Indianapolis IN. Currently have the map displaying through turtle. Working on how to overlay the ISS image onto the map. Also added docstrings.

--- iss.py
+++ iss.py
@@ -3,9 +3,14 @@
 __author__ = 'Meagan Ramey'
 
 import requests
+import turtle
+import time
 
 
 def astronauts():
+    '''Obtains and returns a list of astronauts, the spacecraft they are
+        aboard, and the total number of astronausts in space using
+        NASA's API.'''
     r = requests.get('http://api.open-notify.org/astros.json')
     r = r.json()
     astronaut_list = r['people']
@@ -19,6 +24,7 @@ def astronauts():
 
 
 def iss_location():
+    '''Obtains the current timestamp and coordinates of the ISS.'''
     r = requests.get('http://api.open-notify.org/iss-now.json')
     r = r.json()
     coords = r['iss_position']
@@ -26,11 +32,34 @@ def iss_location():
     return time, coords
 
 
+def over_indy():
+    '''Obtains and returns the next date, to the seconds, the ISS will be over
+        Indianapolis, IN'''
+    r = requests.get(
+        """
+        http://api.open-notify.org/iss/v1/?lat=40.027435&lon=-86.158068&alt=1650&n=1
+        """
+        )
+    r = r.json()
+    date = r['response'][0]['risetime']
+    seconds = r['response'][0]['duration']
+    overhead_date = time.ctime(date)
+    indy_data = [overhead_date, seconds]
+    return indy_data
+
+
+def iss_display(time, coords, indy):
+    disp = turtle.Screen()
+    disp.setup(width=500, height=500)
+    disp.bgpic('map.gif')
+    turtle.done()
+
+
 def main():
-    astronauts_on_ISS = astronauts()
+    astronauts()
     time, coords = iss_location()
-    print(time)
-    print(coords)
+    indy = over_indy()
+    iss_display(time, coords, indy)
 
 
 if __name__ == '__main__':
